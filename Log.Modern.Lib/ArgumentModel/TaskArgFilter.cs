3faa88c3a1b0e534b50f013a10b3b3f3bba67f83
krzm <kmazanek@gmail.com>
2022-02-17 20:30:34 +0100

    Rename folder

---
+++ Log.Modern.Lib/ArgumentModel/TaskArgFilter.cs
@@ -0,0 +1,21 @@
+﻿using CommandDotNet;
+using System.ComponentModel.DataAnnotations;
+
+namespace Log.Modern.Lib;
+
+public class TaskArgFilter : IArgumentModel
+{
+    private const string IdError = "Id must be greater than zero";
+
+    [Option(
+        'c'
+        , nameof(CategoryId))
+        , Range(1, int.MaxValue, ErrorMessage = IdError)]
+    public int? CategoryId { get; set; }
+
+    [Option(
+        'n'
+        , nameof(Name))
+        , MaxLength(25)]
+    public string? Name { get; set; }
+}
