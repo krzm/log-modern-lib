5755a1963a7a9ceabe85b9c39225848085f3fe82
krzm <kmazanek@gmail.com>
2022-06-28 00:13:08 +0200

    Add dep sets

---
+++ Log.Modern.Lib/DependencySet/Unity/AppCommands.cs
@@ -0,0 +1,55 @@
+using CRUDCommandHelper;
+using DIHelper.Unity;
+using Unity;
+
+namespace Log.Modern.Lib.Unity;
+
+public class AppCommands 
+    : UnityDependencySet
+{
+    public AppCommands(
+        IUnityContainer container) 
+        : base(container)
+    {
+    }
+
+    public override void Register()
+    {
+        RegisterReadCommands();
+        RegisterInsertCommands();
+        RegisterUpdateCommands();
+    }
+
+    private void RegisterReadCommands()
+    {
+        Container
+            .RegisterSingleton<IReadCommand<PlaceArgFilter>, PlaceReadCommand>()
+            .RegisterSingleton<IReadCommand<CategoryArgFilter>, CategoryReadCommand>()
+            .RegisterSingleton<IReadCommand<TaskArgFilter>, TaskReadCommand>()
+            .RegisterSingleton<IReadCommand<LogFilterArgs>, LogReadCommand>();
+    }
+
+    private void RegisterInsertCommands()
+    {
+        Container
+            .RegisterSingleton<IInsertCommand<PlaceArg>, PlaceInsertCommand>()
+            .RegisterSingleton<IInsertCommand<CategoryArg>, CategoryInsertCommand>()
+            .RegisterSingleton<IInsertCommand<Lib.TaskArg>, TaskInsertCommand>()
+            .RegisterSingleton<IInsertCommand<Lib.LogArg>, LogInsertCommand>();
+    }
+
+    private void RegisterUpdateCommands()
+    {
+        Container
+            .RegisterSingleton<IUpdateCommand<PlaceArgUpdate>, PlaceUpdateCommand>()
+            .RegisterSingleton<IUpdateCommand<CategoryArgUpdate>, CategoryUpdateCommand>()
+            .RegisterSingleton<IUpdateCommand<TaskArgUpdate>, TaskUpdateCommand>()
+            .RegisterSingleton<IUpdateCommand<LogArgUpdateReset>, LogUpdateCommand>();
+    }
+
+    private void RegisterDeleteCommands()
+    {
+        Container
+            .RegisterSingleton<IDeleteCommand<DeleteArgs>, CategoryDeleteCommand>();
+    }
+}
---
+++ Log.Modern.Lib/DependencySet/Unity/AppMappings.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Log.Data;
+using Unity;
+using Task = Log.Data.Task;
+
+namespace Log.Modern.Lib.Unity;
+
+public class AppMappings 
+    : ModelHelper.AppMappings
+{
+    public AppMappings(
+        IUnityContainer container)
+        : base(container)
+    {
+    }
+
+    protected override MapperConfiguration CreateMap() => 
+        new (
+        c =>
+        {
+            c.CreateMap<PlaceArg, Place>();
+            c.CreateMap<CategoryArg, Category>();
+            c.CreateMap<TaskArg, Task>();
+            c.CreateMap<LogArg, LogModel>();
+
+            c.CreateMap<PlaceArgUpdate, PlaceUpdate>();
+            c.CreateMap<CategoryArgUpdate, CategoryUpdate>();
+            c.CreateMap<TaskArgUpdate, TaskUpdate>();
+            c.CreateMap<LogArgUpdateReset, LogUpdate>();
+        });
+}
